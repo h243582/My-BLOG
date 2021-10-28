a3ebb57465c56a2b2b7331a3897c04c8ec00ec3b
heyufei6 <Hh243582>
2021-10-28 17:22:34 +0800

    10.28

--- hyf_web/src/main/java/cm/hyf/web/Web.java
+++ hyf_web/src/main/java/cm/hyf/web/Web.java
@@ -1,20 +1,13 @@
-package com.hyf.manager;
+package cm.hyf.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import util.JwtUtil;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class Manager {
+public class Web {
     public static void main(String[] args) {
-        SpringApplication.run(Manager.class, args);
-    }
-
-    @Bean
-    public JwtUtil jwtUtil(){
-        return new JwtUtil();
+        SpringApplication.run(Web.class);
     }
 }
